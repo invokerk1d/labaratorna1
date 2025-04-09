0a62a9568636efd07a8a9c184e8e2200bbcea3a5
Zakhar <zackhar21@gmail.com>
2025-04-09 18:59:44 +0300

    fix

--- build.bat
+++ build.bat
@@ -1,4 +1,12 @@
 @echo off
-python -m pyinstaller -F -i "icon.ico" lab1.py
+REM Встановлюємо залежності
+C:\Users\psyhok1d\AppData\Local\Microsoft\WindowsApps\pip.exe install -r requirements.txt
+
+REM Запускаємо тести
+C:\Users\psyhok1d\AppData\Local\Microsoft\WindowsApps\python.exe -m unittest test_calculator.py
+
+REM Збираємо .exe
+C:\Users\psyhok1d\AppData\Local\Microsoft\WindowsApps\python.exe -m pyinstaller -F -i "icon.ico" lab1.py
+
 echo Build completed!
 pause
