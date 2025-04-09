4ae3713134f518d60ac2ee2f4cefa1126a5b25bc
Zakhar <zackhar21@gmail.com>
2025-04-09 19:17:49 +0300

    fixed

--- build.bat
+++ build.bat
@@ -1,16 +1,12 @@
 @echo off
-REM Перевірка Python і pip
-python --version || exit /b 1
-pip --version || exit /b 1
-
 REM Встановлення залежностей
-pip install -r requirements.txt || exit /b 1
+python -m pip install -r requirements.txt || exit /b 1
 
 REM Запуск тестів
 python -m unittest test_calculator.py || exit /b 1
 
 REM Збірка .exe
-pyinstaller -F -i "icon.ico" lab1.py || exit /b 1
+python -m pyinstaller -F -i "icon.ico" lab1.py || exit /b 1
 
 echo Build completed!
 pause
