103290bff8821a795a230e0ae52b4524a7db2791
Zakhar <zackhar21@gmail.com>
2025-04-09 19:57:08 +0300

    fix

--- build.bat
+++ build.bat
@@ -10,8 +10,6 @@ REM Запускаємо тести
 echo Running tests...
 "C:\Program Files\Python\python.exe" -m unittest test_calculator.py
 
-REM Створення .exe файлу
-echo Building executable...
-"C:\Program Files\Python\Scripts\pyinstaller.exe" -F -i "icon.ico" lab1.py
+
 
 echo Build completed!
