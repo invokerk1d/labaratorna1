8ac77d81045ad8235bb31e2d908beb2eef2aa9ca
Zakhar <zackhar21@gmail.com>
2025-04-09 19:27:28 +0300

    fixedd

--- build.bat
+++ build.bat
@@ -1,12 +1,8 @@
 @echo off
-REM Встановлення залежностей
-python -m pip install -r requirements.txt || exit /b 1
-
-REM Запуск тестів
-python -m unittest test_calculator.py || exit /b 1
-
-REM Збірка .exe
-python -m pyinstaller -F -i "icon.ico" lab1.py || exit /b 1
+REM Використовуємо конкретний шлях до Python
+"C:\Program Files\Python\python.exe" -m pip install -r requirements.txt || exit /b 1
+"C:\Program Files\Python\python.exe" -m unittest test_calculator.py || exit /b 1
+"C:\Program Files\Python\python.exe" -m pyinstaller -F -i "icon.ico" lab1.py || exit /b 1
 
 echo Build completed!
 pause
